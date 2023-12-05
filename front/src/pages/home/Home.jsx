241c83f05729bc666d8053429af1b3cad56086c8
Amira Ramzi <ramzi70174@gmail.com>
2023-12-05 20:09:58 +0100

    navbar

---
+++ front/src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './home.css'
+import Navbar from '../../Components/navbar/Navbar'
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Home
